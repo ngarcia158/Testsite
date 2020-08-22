a5ffa20affd6157f664e64cc767bb3ebfc175bd7
Nicholas Garcia <ngarcia158@west-mec.org>
2020-08-21 20:52:31 -0700

    These are the changes ive made
    I was doing a lot of testing with javascript buttons hope to work on them tm

---
+++ Index.js
@@ -0,0 +1,13 @@
+"use strict"
+//when clicked will cahnge text under to show a table of how good i am in that section and some things i knoiw what to do in it
+function htmlGrade(){
+    
+}
+
+function cssGrade(){
+    
+}
+
+function javaScriptGrade(){
+    
+}
